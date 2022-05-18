3dbc8d6bfcfb973424cfaedb1d89632cb7776708
Blastose <blastoisejay@gmail.com>
2022-05-18 12:31:31 -0700

    Update about page text

--- src/about.js
+++ src/about.js
@@ -12,7 +12,9 @@ const about = (() => {
 
   const _aboutContent = DomManipulation.createElementWithClass('div', 'about-content');
   _aboutContent.innerText = 
-  `Founded in 1902, Restaurant was the creation of Jimmy's brilliant idea. Ever since it's inception, Restaurant has served millions of customers with only the finest pizzas man has ever created. 
+  `Founded in 1902, Restaurant was the creation of Jimmy's brilliant idea. As of 2022, Restaurant has over 5 million restaurants across North Ameraica, with plans to expand in other parts of the world. 
+  
+  Ever since it's inception, Restaurant has served millions of customers with only the finest pizzas man has ever created. Each pizza is carefully crafted with the loving touch our of highly skilled chefs. 
   
   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mauris quam, faucibus quis egestas at, faucibus in risus. Quisque at faucibus mauris, mattis auctor ligula. Phasellus consectetur, lectus et egestas commodo, lectus sapien efficitur mauris, nec fermentum erat ex non nisl. Ut blandit orci vestibulum ante fermentum, at lacinia justo sollicitudin. Donec non enim ut lorem accumsan fermentum in id ipsum. Fusce facilisis imperdiet interdum. Maecenas laoreet consequat imperdiet. Fusce aliquam sem eget odio rhoncus accumsan. Donec luctus sagittis porttitor. Fusce vitae facilisis justo. Aliquam erat volutpat. Suspendisse a tempor ipsum. Praesent condimentum dui non fringilla ornare.`;
   
